f5cf6cf29487eb1e0e2745bf983b5866004b01ea
Ivan Lesko <ivanlesko+github@gmail.com>
2024-07-12 14:16:34 -0700

    Fill out the checkout view form

--- SwiftUILearning/CheckoutView.swift
+++ SwiftUILearning/CheckoutView.swift
@@ -6,22 +6,31 @@
 //
 
 import SwiftUI
+import Combine
 
 struct CheckoutView: View {
     private enum PaymentType: String, CaseIterable {
         case cash = "Cash"
         case card = "Credit card"
         case points = "Points"
+        case giftCard = "Gift card"
+        case virtual = "Virtual"
     }
     
     @EnvironmentObject var order: Order
     
     private let paymentTypes = PaymentType.allCases
+    private let tipAmounts = [5, 10, 15, 20, 0]
     
     @State private var paymentType = PaymentType.cash
+    @State private var tipAmount = 15
+    @State private var timer: AnyCancellable?
+    
+    @State private var addLoyaltyDetails = false
+    @State private var loyaltyNumber = ""
     
     var body: some View {
-        VStack {
+        Form {
             Section {
                 Picker("Select payment type", selection: $paymentType) {
                     ForEach(paymentTypes, id: \.self) { payment in
@@ -29,17 +38,55 @@ struct CheckoutView: View {
                             .tag(payment)
                     }
                 }
-                .pickerStyle(.wheel)
+                .animation(.easeOut, value: paymentType)
+                
+                Toggle("Use loyalty card", isOn: $addLoyaltyDetails)
+                
+                if addLoyaltyDetails {
+                    TextField("Enter custom ID", text: $loyaltyNumber)
+                }
             }
             
+            Section("Add a tip?") {
+                Picker("Percent:", selection: $tipAmount) {
+                    ForEach(tipAmounts, id: \.self) {
+                        Text("\($0)%")
+                    }
+                }
+            }
             
-            Spacer()
+            Section("Total: $0") {
+                Button("Place Order") {
+                    // place order here
+                }
+            }
         }
         .navigationTitle("Payment")
         .navigationBarTitleDisplayMode(.inline)
-        .onChange(of: paymentType) { newValue in
-            print("payment type changed: \(newValue)")
+        .onAppear {
+            startTimer()
         }
+        .onDisappear {
+            stopTimer()
+        }
+    }
+    
+    private func startTimer() {
+        timer = Timer.publish(every: 1.0, on: .main, in: .common)
+            .autoconnect()
+            .sink(receiveValue: { _ in
+                let types = PaymentType.allCases
+                
+                if let index = types.firstIndex(of: paymentType) {
+                    let next = (index + 1) % types.count
+                    
+                    paymentType = types[next]
+                }
+            })
+    }
+    
+    private func stopTimer() {
+        
     }
 }
 
